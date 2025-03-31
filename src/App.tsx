e1839c91e30c05b8990c00a2e7383c41f7f25ea6
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-31 19:27:34 +0500

    added transitions with its duration to zooming

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
             <div
                 className={`${frameZoom && 'min-w-[97vw] min-h-[97vw]'}'
               w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw]
-              max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative
+              max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-500
             '`}>
                 <Controls frameZoom={frameZoom} toggleFrameZoom={toggle}/>
             </div>
