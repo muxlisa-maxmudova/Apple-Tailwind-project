5ab6c3d7a19b4bed0dd7a1f80b55fa8ade08d8a6
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-31 19:24:26 +0500

    zooming controls done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import Controls from "./Controls.tsx";
+import {useState} from "react";
+
 const App = () => {
+    const [frameZoom, setFrameZoom]= useState<boolean>(false)
+    const toggle = () => {
+        setFrameZoom(!frameZoom)
+    }
     return (
         <div className='w-full h-screen grid place-items-center'>
             <div
-                className='
+                className={`${frameZoom && 'min-w-[97vw] min-h-[97vw]'}'
               w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw]
-              max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto
-            '>
+              max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative
+            '`}>
+                <Controls frameZoom={frameZoom} toggleFrameZoom={toggle}/>
             </div>
 
         </div>
---
+++ src/Controls.tsx
@@ -0,0 +1,14 @@
+const Controls = ({frameZoom, toggleFrameZoom}:{frameZoom: boolean, toggleFrameZoom:()=>void}) => {
+    return (
+        <div className='absolute top-3 right-3 space-x-2'>
+            <button className='text-2xl text-blue-500 cursor-pointer' onClick={toggleFrameZoom}>
+                <i className={frameZoom?'bx bxs-zoom-out':'bx bxs-zoom-in'}></i>
+            </button>
+            <button className='text-2xl text-blue-500 cursor-pointer'>
+                <i className='bx bxs-x-circle'></i>
+            </button>
+        </div>
+    )
+}
+export default Controls
+
