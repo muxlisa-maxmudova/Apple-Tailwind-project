fd484aace6f9ac2a22d4e67acdc2ed5326dca431
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-31 18:56:00 +0500

    box set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 const App = () => {
     return (
-        <div>
+        <div className='w-full h-screen grid place-items-center'>
+            <div
+                className='
+              w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw]
+              max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto
+            '>
+            </div>
 
         </div>
     )
